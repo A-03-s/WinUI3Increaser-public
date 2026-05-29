e886779fc37f7dffc5e9cbe27956ec783a76518f
Ash <lab.a03s.ar@gmail.com>
2026-05-29 14:21:07 -0300

    Commit N°1: Separamos el modelo en una clase independiente ubicada en una nueva carpeta que denominamos "Models". Además, adecuamos el ViewModel y la View a dicho cambio:  - Exponemos la propiedad _counter para la View, creando "ValorUI" y a su vez, notificamos a la View cada cambio de esta propiedad agregando la línea:         OnPropertyChanged(nameof(ValorUI));    Esta es una directiva del CommunityToolkit para avisar al x:Bind.  - En la View, biendeamos a la property "ValorUI" de ViewModel:        <TextBlock Text="{x:Bind ViewModel.ValorUI, Mode=OneWay}"

---
+++ WinUI3Increaser/Models/IncreaserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WinUI3Increaser.Models
+{
+    public class IncreaserModel
+    {
+        // El dato puro
+        public int Valor { get; set; } = 0;
+
+        // Lógica de negocio pura
+        public void IncrementarValor()
+        {
+            Valor++;
+        }
+    }
+}
